b7df392352d2d79fb5e970c767244e517d239042
Changhogod <younch8342@naver.com>
2023-03-18 13:05:34 +0900

    MemberRepositoryTest

--- src/test/java/com/changddao/SpringDataJPA/repository/MemberRepositoryTest.java
+++ src/test/java/com/changddao/SpringDataJPA/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -25,6 +26,34 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("changho");
+        Member member2 = new Member("jina");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> allMember = memberRepository.findAll();
+        assertThat(allMember.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long afterDeleteCnt = memberRepository.count();
+        assertThat(afterDeleteCnt).isEqualTo(0);
     }
 
 }
